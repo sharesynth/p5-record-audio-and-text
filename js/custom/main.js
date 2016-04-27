807c7e085cbc9ecb40e8d64f15edba1358a77cc0
Jeremy Sykes <jsykes@tvo.org>
2016-04-27 17:00:22 -0400

    Removed comments

--- js/custom/main.js
+++ js/custom/main.js
@@ -6,66 +6,44 @@ var state = 0;
 var speech;
 
 function setup() {
-
-    // Setup graphics
     createCanvas(400, 400);
     background(200);
-    fill(0);
-    text("Click to start recording (Wear headphones and have your mic enabled).", 10, 20);
-
-    // Setup voice recorder
+    fill(color('White'));
+    textSize(32);
+    textAlign(CENTER);
+    text('Click to start recording.\n(Wear headphones to \navoid nasty feedback)', width / 2, height / 2);
     mic = new p5.AudioIn();
     mic.start();
     recorder = new p5.SoundRecorder();
     recorder.setInput(mic);
     soundFile = new p5.SoundFile();
-
-    // Setup speech recorder
     speech = new p5.SpeechRec();
 }
 
 function mousePressed() {
     if (state === 0 && mic.enabled) {
 
-        // Start the recorder
         recorder.record(soundFile);
-
-        // Update the graphics
         background(255, 0, 0);
-        text('Recording now! Click to stop.', 10, 20);
-
-        // Start recording the speech
+        text('Recording now! \nClick to stop.', width / 2, height / 2);
         speech.continuous = true;
         speech.start();
-
-        // Increment state
         state++;
 
     } else if (state === 1) {
 
-        // Stop the recorder
         recorder.stop();
-
-        // Update the graphics
         background(0, 255, 0);
-        text('Recording stopped. Click to play & save', 10, 20);
-
-        // Stop recording the speech
+        text('Recording stopped. \nClick to play & save', width / 2, height / 2);
         speech.continuous = false;
-
-        // Increment the state
         state++;
 
     } else if (state === 2) {
 
-        // Play and save the sound file
         soundFile.play();
         saveSound(soundFile, 'soundFile.wav');
-
-        // Display and save the text
         save(speech.resultString, 'soundTranscript.txt');
-
-        // Increment the state
+        text(speech.resultString, width / 2, height / 2);
         state++;
     }
 }
